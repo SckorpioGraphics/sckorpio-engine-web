473bb4f2a9e2a9a853f93e68af0b5894aa6e5323
sckorpio <kartik.sckorpio@gmail.com>
2026-06-10 02:22:38 +0530

    Add instances support in meshComponent

--- sckorpioWebEngine/core/ecs/componentList/meshComponent.js
+++ sckorpioWebEngine/core/ecs/componentList/meshComponent.js
@@ -4,12 +4,24 @@ import { RenderComponent } from "./renderComponent.js";
 class MeshComponent extends Component{
     constructor() {
         super();
+
+        //source mesh data
         this.visible = true;
-        this.layout;
+        this.vertexLayout;
         this.vertexData = [];
         this.indexData = [];
+
+        //instances data
+        this.isInstanced = false;
+        this.instanceLayout;
+        this.instanceData = [];
+        this.instancesCount = 0;
+
+        //material data
         this.textureUV = [0.0, 0.0, 1.0, 1.0];
         this.material;
+
+        //render component(gpu)
         this.renderComponent = new RenderComponent();
     }
 
@@ -29,8 +41,21 @@ class MeshComponent extends Component{
         this.indexData = indexData;
     }
 
-    setLayout(layout){
-        this.layout = layout;
+    setVertexLayout(layout){
+        this.vertexLayout = layout;
+    }
+
+    setInstanced(){
+        this.isInstanced = true;
+        const defaultInstanceLayout = [
+            { name: "a_instanceMatrix0", type: "mat4f" }
+        ];
+        this.setInstanceLayout(defaultInstanceLayout);
+    }
+
+    setInstanceLayout(layout){
+        this.isInstanced = true;
+        this.instanceLayout = layout;
     }
 
     setTextureRepeat(repeatX,repeatY){
@@ -54,8 +79,51 @@ class MeshComponent extends Component{
         this.renderComponent.unbind();
     }
 
+    createInstance(
+        position = [0,0,0],
+        rotation = [0,0,0],
+        scale = [1,1,1]
+    ){
+        if(!this.isInstanced){
+            this.setInstanced();
+        }
+
+        // cretae Identity matrix
+        let instanceModelMatrix = mat4.create();
+        // Apply translation
+        mat4.translate(instanceModelMatrix, instanceModelMatrix, position);
+        
+        // Create rotation matrix
+        let rotationMatrix = mat4.create();
+        mat4.rotateX(rotationMatrix, rotationMatrix, glMatrix.toRadian(rotation[0])); // Rotation around X axis
+        mat4.rotateY(rotationMatrix, rotationMatrix, glMatrix.toRadian(rotation[1])); // Rotation around Y axis
+        mat4.rotateZ(rotationMatrix, rotationMatrix, glMatrix.toRadian(rotation[2])); // Rotation around Z axis
+
+        // Apply rotation
+        mat4.multiply(instanceModelMatrix, instanceModelMatrix, rotationMatrix);
+
+        // Apply scaling
+        mat4.scale(instanceModelMatrix, instanceModelMatrix, scale);
+        
+        // Push the 16 raw matrix floats directly into our loose CPU array
+        for (let i = 0; i < 16; i++) {
+            this.instanceData.push(instanceModelMatrix[i]);
+        }
+        this.instancesCount++;
+
+    }
+
     loadGPUData(){
+        // set material
+        this.renderComponent.setMaterial(this.material);
+        // set Source Mesh Data
         this.renderComponent.setData(this.layout,this.vertexData,this.indexData);
+        // set Instances Data
+        if(this.isInstanced){
+            if(this.instanceData.length > 0){
+                this.renderComponent.setInstancedData(this.instanceLayout, this.instanceData, this.instancesCount);
+            }
+        }
     }
 }
 
