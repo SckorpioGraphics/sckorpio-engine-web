adcb3905bbf452613e1fe0ba154ec92f0c0c666c
sckorpio <kartik.sckorpio@gmail.com>
2026-08-13 23:59:07 +0530

    Added Scene Graph Parent/Child mechanism in Node class

--- sckorpioEngineWeb/core/ecs/entityList/node/node.js
+++ sckorpioEngineWeb/core/ecs/entityList/node/node.js
@@ -6,12 +6,51 @@ class Node extends Entity{
         super();
         this.transformComponent = null;
         this.addTransformComponent();
+
+        // instancing
         this.instanced = false;
+
+        // Scene Graph
+        this.parentNode = null;
+        this.childNodes = [];
+        this.depth = 0;
     }
 
     addTransformComponent(){
         this.transformComponent = new TransformComponent();
         this.components.push(this.transformComponent);
+    }
+
+    setParent(parentNode){
+        // If parent is already there
+        if (this.parentNode === parentNode) return;
+
+        // set Parent
+        this.parentNode = parentNode;
+
+        if(parentNode instanceof Node) {
+            // add this as child to parent
+            parentNode.addChild(this);
+            // add parentTransformComponent
+            this.transformComponent.setParent(parentNode.transformComponent);
+            // update depth
+            this.updateDepth(this.parentNode.depth + 1);
+        }
+    }
+
+    addChild(childNode){
+        // If child is already there
+        if (!this.childNodes.includes(childNode)) {
+            this.childNodes.push(childNode);
+            childNode.setParent(this);
+        }
+    }
+
+    updateDepth(newDepth){
+        this.depth = newDepth;
+        this.childNodes.forEach((childNode)=>{
+            childNode.updateDepth(this.depth+1);
+        });
     }
 
     setPosition(x,y,z){
