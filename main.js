6a0b18d875bce37a32509d7ecda44df3b683cdf1
sckorpio <kartik.sckorpio@gmail.com>
2026-08-18 13:46:29 +0530

    Set Main: IndependeDay Scene

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 import { verifyWebGLSupport } from "./sckorpioEngineWeb/canvas/utils.js";
 import { title } from "./sckorpioEngineWeb/canvas/title.js";
-import { Scene } from "./projects/testing3SceneGraph/scene.js";
+import { Scene } from "./projects/projectIndependenceDay/scene.js";
 
 var initSckorpioWebEngine = async function () {
     //Verify WebGL Support
     verifyWebGLSupport();
 
     // Initialize the scene
-    var scene = new Scene("testing3SceneGraph"); 
+    var scene = new Scene("projectIndependenceDay"); 
     await scene.init(); 
     await scene.initResources();
     await scene.createScene();
