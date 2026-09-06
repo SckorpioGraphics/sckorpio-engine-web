2d9723e6273d223fe0a81e2665a635f221398b98
sckorpio <kartik.sckorpio@gmail.com>
2026-09-07 04:42:37 +0530

    Cleaned: Main

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
 //######## PROJECT ####################
-const project = "./sckorpioProject";
+const project = "./projects/testing1Basic";
 //#####################################
 // Default: project = "./sckorpioProject";
 
-import { verifyWebGLSupport } from "./sckorpioEngine/canvas/utils.js";
-import { title } from "./sckorpioEngine/canvas/title.js";
 const { Scene } = await import(project + "/scene.js");
 
-var initSckorpioEngine = async function () {
-    //Verify WebGL Support
-    verifyWebGLSupport();
-
+var setProject = async function () {
     // Initialize the scene
     var scene = new Scene(project); 
-    await scene.init(); 
-    await scene.initResources();
-    await scene.createScene();
-    scene.load();
-    scene.play();
+    await scene.init();
 }
 
-initSckorpioEngine();
+setProject();
